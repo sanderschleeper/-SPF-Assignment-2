148d338ff663f6c06290b8ad77aa4ad651952380
sanderschleeper <sanderboy987@gmail.com>
2020-10-07 12:08:07 +0200

    Delete simpathfuncb.m

--- Computer assignment 2/simpathfuncb.m
+++
@@ -1,8 +0,0 @@
-function [ST] = simpathfuncb(r, sigma, M, S0,T)
-ST = NaN(M,1);
-
-    for i = 1:M
-        rnum = randn;
-        ST(i) = S0 * exp((r-0.5*sigma^2)*T + sigma*sqrt(T)*rnum);
-    end
-end
